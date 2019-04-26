3c284b08db8d853155f907ac65c490a8483fd24e
Amidasd <amidasd@gmail.com>
2019-04-26 07:49:10 +0000

    Delete global_lang.php

--- application/language/german/global_lang.php
+++
@@ -1,6 +0,0 @@
-<?php
-$lang['global_title'] = "Hallo";
-$lang['global_email'] = "Email";
-$lang['global_password'] = "Passwort";
-$lang['global_enter'] = "Eingeben";
-$lang['global_registration'] = "Anmeldung";
