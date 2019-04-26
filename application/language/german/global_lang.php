e561ad27ecc7287b60670fd628219f744175e10f
Amidasd <amidasd@gmail.com>
2019-04-26 10:53:58 +0300

    Update global_lang.php

--- application/language/german/global_lang.php
+++ application/language/german/global_lang.php
@@ -1,6 +1,28 @@
 <?php
-$lang['global_title'] = "Hallo";
-$lang['global_email'] = "Email";
-$lang['global_password'] = "Passwort";
-$lang['global_enter'] = "Eingeben";
-$lang['global_registration'] = "Anmeldung";
+
+$lang['global_home_registration_info_all_fields'] = "Все поля являются обязательными для заполнения.";
+$lang['global_home_registration_your_email'] = "Ваш email";
+$lang['global_home_registration_enter_your_email'] = "Введите email";
+$lang['global_home_registration_your_password'] = "Ваш пароль";
+$lang['global_home_registration_enter_password'] = "Введите пароль";
+$lang['global_home_registration_repeat_password'] = "Повторите пароль";
+$lang['global_home_registration_your_name'] = "Ваше имя";
+$lang['global_home_registration_enter_your_name'] = "Введите имя";
+$lang['global_home_registration_cancel'] = "Отмена";
+$lang['global_home_forgot_your_password'] = "Забыли пароль?";
+$lang['global_home_registration_error_name'] = "Имя должно содержать от 2 до 20 символов кроме цифр";
+$lang['global_home_registration_error_name_space'] = "Поле имя не должно быть пустым!";
+$lang['global_home_registration_error_password'] = "Пароль должен быть не менее 4 и не более 20 символов и содержать только строчные и прописные латинские буквы и цифры";
+$lang['global_home_registration_error_password_space'] = "Поле пароль не должно быть пустым!";
+$lang['global_home_registration_error_password2'] = "Пароли не совпадают";
+$lang['global_home_registration_error_email'] = "Email уже зарегистрирован";
+$lang['global_home_registration_error_email_incorrect'] = "Email некорректен";
+$lang['global_home_registration_error_email_space'] = "Поле email не должно быть пустым!";
+$lang['global_home_incorrect_auth']="Не верно введены имя пользователя или пароль, либо вы еще не активировали учетную запись";
+$lang['global_home_projects'] = "Проект";
+$lang['global_home_projects_enter_your_project'] = "Введите название проекта";
+$lang['global_home_projects_create'] = "Создать проект";
+$lang['global_home_projects_fields'] = "Поле проект не заполненно";
+$lang['global_home_projects_error'] = "Имя должно содержать от 1 до 255 любых символов";
+$lang['global_home_create_task'] = "Start typing here to create a task...";
+$lang['global_home_add_task'] = "Add Task";
